b88c55029921159b8af64146c69c6e63f6518019
Ya.nnis <ybarache@icloud.com>
2025-01-30 14:41:45 +0100

    ✨#1 : Fin ajout du ranking

--- apps/realtime-elo-ranker-server/src/ranking/ranking.module.ts
+++ apps/realtime-elo-ranker-server/src/ranking/ranking.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RankingService } from './ranking.service';
 import { RankingController } from './ranking.controller';
+import { PlayerModule } from '../player/player.module';
 
 @Module({
+  imports: [PlayerModule],
   providers: [RankingService],
+  exports: [RankingService],
   controllers: [RankingController],
-
 })
 export class RankingModule {}
